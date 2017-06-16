2df13cdc4cc1c2bc5a308bfb32fde2cf1e6fbffb
Gweltaz GESTIN <gweltaz.gestin@capgemini.com>
2017-06-16 23:32:28 +0200

    init

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+  ],
+  exports: [
+  ]
+})
+export class SampleModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: SampleModule,
+      providers: []
+    };
+  }
+}
